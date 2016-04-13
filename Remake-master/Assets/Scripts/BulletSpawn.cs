63c1ca57b27e8e453b0557106e1efc77af698d35
Thomas Santopadre <Thomas Santopadre>
2016-04-13 11:21:46 -0500

    First commit

---
+++ Remake-master/Assets/Scripts/BulletSpawn.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletSpawn : MonoBehaviour {
+
+    public GameObject bullet, bulletSpawn;
+    public int chargeTime;
+    public bool fired;
+
+    private PlayerMove player;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindObjectOfType<PlayerMove>();
+        chargeTime = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if ((Input.GetKeyDown(KeyCode.Z) || (Input.GetAxis("RT") > 0)) && !fired)
+        {
+            fired = true;
+            Instantiate(this.bullet, bulletSpawn.transform.position, bulletSpawn.transform.rotation);
+        }
+        if (Input.GetKeyUp(KeyCode.Z) || (Input.GetAxis("RT") < 1))
+        {
+            fired = false;
+        }
+	}
+}
---
+++ Remake-master/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,123 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour {
+
+    public GameObject DLStart, DRStart, DLEnd, DREnd;
+    public float speed;
+    public float jumpForce;
+    public bool isGrounded;
+    public bool facingRight;
+    public bool dashed;
+
+    private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        speed = 6.0f;
+        jumpForce = 3000.0f;
+        facingRight = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RayCast();
+        Jump();
+        Dash();
+	}
+
+    void FixedUpdate()
+    {
+        MoveHoriz();
+    }
+
+    void RayCast()
+    {
+        if (Physics2D.Linecast(DLStart.transform.position, DLEnd.transform.position) || Physics2D.Linecast(DRStart.transform.position, DREnd.transform.position))
+        {
+            dashed = false;
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }
+
+    void MoveHoriz()
+    {
+        if (Input.GetAxis("Horizontal") <= -0.3f)
+        {
+            facingRight = false;
+            transform.localScale = new Vector3(-1, 1, 1);
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+        if (Input.GetAxis("Horizontal") >= 0.3f)
+        {
+            facingRight = true;
+            transform.localScale = new Vector3(1, 1, 1);
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
+        }
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.AddForce(Vector2.up * jumpForce);
+        }
+    }
+
+    void Dash()
+    {
+        if (!dashed && !isGrounded && Input.GetButtonDown("Jump") && Input.GetAxis("Horizontal") >= 0.3f)
+        {
+            dashed = true;
+            StartCoroutine(DashRight());
+        }
+        if (!dashed && !isGrounded && Input.GetButtonDown("Jump") && Input.GetAxis("Horizontal") <= -0.3f)
+        {
+            dashed = true;
+            StartCoroutine(DashLeft());
+        }
+    }
+
+    IEnumerator DashRight()
+    {
+        rb.velocity = new Vector3(0, 0, 0);
+        Vector2 grav = Physics2D.gravity;
+        grav.y = 0;
+        Physics2D.gravity = grav;
+        Vector3 dashPos = transform.position;
+        dashPos.x += 4.0f;
+        while (transform.position.x <= dashPos.x)
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * speed * 2.5f);
+            yield return new WaitForSeconds(0);
+        }
+        dashPos.y = transform.position.y;
+        transform.position = dashPos;
+        grav.y = -9.81f;
+        Physics2D.gravity = grav;
+    }
+
+    IEnumerator DashLeft()
+    {
+        rb.velocity = new Vector3(0, 0, 0);
+        Vector2 grav = Physics2D.gravity;
+        grav.y = 0;
+        Physics2D.gravity = grav;
+        Vector3 dashPos = transform.position;
+        dashPos.x -= 4.0f;
+        while (transform.position.x >= dashPos.x)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed * 2.5f);
+            yield return new WaitForSeconds(0);
+        }
+        dashPos.y = transform.position.y;
+        transform.position = dashPos;
+        grav.y = -9.81f;
+        Physics2D.gravity = grav;
+    }
+}
